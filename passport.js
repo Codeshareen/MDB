5712338c1bcd549df1977447cf98767e4a0bc5ad
codeshareen <study78160@gmail.com>
2024-07-25 21:04:05 +0530

    gmailcs

--- passport.js
+++ passport.js
@@ -1,22 +1,29 @@
 const passport = require('passport'); 
 const GoogleStrategy = require('passport-google-oauth2').Strategy; 
 
-passport.serializeUser((user , done) => { 
-	done(null , user); 
-}) 
-passport.deserializeUser(function(user, done) { 
-	done(null, user); 
-}); 
+passport.serializeUser((user, done) => { 
+    done(null, user); 
+});
+
+passport.deserializeUser((user, done) => { 
+    done(null, user); 
+});
 
 passport.use(new GoogleStrategy({ 
-	clientID:process.env.CLIENT_ID, // Your Credentials here. 
-	clientSecret:process.env.CLIENT_SECRET, // Your Credentials here. 
-	callbackURL:"https://mdb-eqr8.onrender.com/auth/google/callback", 
-	passReqToCallback:true
+    clientID: process.env.CLIENT_ID, // Your Client ID here 
+    clientSecret: process.env.CLIENT_SECRET, // Your Client Secret here 
+    callbackURL: "https://mdb-eqr8.onrender.com/auth/google/callback", 
+    passReqToCallback: true,
+    // Add the following option to restrict to your domain
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo' // Google API endpoint
 }, 
 function(request, accessToken, refreshToken, profile, done) { 
-	return done(null, profile); 
-} 
-));
-
-
+    // Check if user's email domain is from adityatekkali.edu
+    const emailDomain = profile.emails[0].value.split('@')[1];
+    if (emailDomain === 'adityatekkali.edu') {
+        return done(null, profile);
+    } else {
+        // Return an error indicating unauthorized domain
+        return done(new Error('Unauthorized domain'));
+    }
+}));
